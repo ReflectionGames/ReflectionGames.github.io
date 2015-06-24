92ac79cdf8e7265037d0d52da320e87cd7239a18
rdfriedl <rdfriedl@gmail.com>
2015-06-24 12:53:05 -0500

    added search page

---
+++ js/searchGames.js
@@ -0,0 +1,35 @@
+var searchGames = function(games) {
+    return function findMatches(q, cb) {
+        var matches, substringRegex;
+
+        // an array that will be populated with substring matches
+        matches = [];
+
+        // regex used to determine if a string contains the substring `q`
+        substrRegex = new RegExp(q, 'i');
+
+        // iterate through the pool of strings and for any string that
+        // contains the substring `q`, add it to the `matches` array
+        $.each(games, function(i, game) {
+            if (substrRegex.test(game.title)) {
+                matches.push(game.title);
+            }
+        });
+
+        cb(matches);
+    };
+};
+ 
+$(document).ready(function() {
+    utils.loadGames(function(games){
+        $('#searchGames').typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 1
+        },
+        {
+            name: 'states',
+            source: searchGames(games)
+        });
+    })
+});
---
+++ js/utils.js
@@ -0,0 +1,47 @@
+utils = {
+	loadGames: function(cb){
+		if(!utils.loadGames.cache){
+			$.ajax({
+				url: '/data/games.json',
+				type: 'get',
+				dataType: 'json'
+			})
+			.done(function(json) {
+				utils.loadGames.cache = json;
+				if(cb) cb(json);
+			})
+			.fail(function() {
+				if(cb) cb(false);
+			})
+		}
+		else{
+			if(cb) cb(utils.loadGames.cache);
+		}
+	},
+	loadGame: function(id,cb){
+		utils.loadGames(function(games){
+			for (var i = 0; i < games.length; i++) {
+				if(games[i].id === id){
+					if(cb) cb(games[i]);
+					return;
+				}
+			};
+			if(cb) cb(false);
+		})
+	},
+	parseSearch: function(url){
+		if(!utils.parseSearch.cache){
+			url = url || location.search;
+			var queries = url.replace(/^\?/, '').replace(/\+/g,' ').split('&');
+			utils.parseSearch.cache = {};
+		    for( i = 0; i < queries.length; i++ ) {
+		        split = queries[i].split('=');
+		        utils.parseSearch.cache[split[0]] = window.unescape(split[1]);
+		    }
+		}
+	    return utils.parseSearch.cache;
+	},
+	serverError: function(id,message,back){
+		location.replace('/errors/'+id+'.html?message='+message+'&back='+back);
+	}
+}
